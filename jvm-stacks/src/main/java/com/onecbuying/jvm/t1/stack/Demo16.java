b67806f0a20e20e1bd60d45254bdce38423cf578
xiaosu <2257346739@qq.com>
2022-08-14 17:43:19 +0800

    演示cpu占用过高，用一些命令查询哪个命令导致线程过高

---
+++ jvm-stacks/src/main/java/com/onecbuying/jvm/t1/stack/Demo16.java
@@ -0,0 +1,42 @@
+package com.onecbuying.jvm.t1.stack;
+
+/**
+ * @author 演示cpu
+ * @version 1.0
+ * @ClassName Demo16
+ * @company 公司
+ * @Description 演示 cpu 占用过高
+ * 1、用top定位哪个进程对cpu的占用过高
+ * 2、ps H -eo pid,tid,%cpu | grep 进程id (用ps命令进一步定位是哪个线程引起的cpu占用过高)
+ * 3、jstack 进程id
+ * @createTime 2022年08月14日 17:41:41
+ */
+public class Demo16 {
+    public static void main(String[] args) {
+        new Thread(null, () -> {
+            System.out.println("1...");
+            while(true) {
+
+            }
+        }, "thread1").start();
+
+
+        new Thread(null, () -> {
+            System.out.println("2...");
+            try {
+                Thread.sleep(1000000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "thread2").start();
+
+        new Thread(null, () -> {
+            System.out.println("3...");
+            try {
+                Thread.sleep(1000000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "thread3").start();
+    }
+}

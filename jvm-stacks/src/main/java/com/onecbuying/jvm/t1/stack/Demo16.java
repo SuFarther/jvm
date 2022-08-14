e80cde133e24329f7fb73de09ef9e74bd0d1ccae
xiaosu <2257346739@qq.com>
2022-08-14 17:51:10 +0800

    演示cpu过高，通过虚拟机通过命令找到这个类出现问题的代码

--- jvm-stacks/src/main/java/com/onecbuying/jvm/t1/stack/Demo16.java
+++ jvm-stacks/src/main/java/com/onecbuying/jvm/t1/stack/Demo16.java
@@ -9,17 +9,19 @@ package com.onecbuying.jvm.t1.stack;
  * 1、用top定位哪个进程对cpu的占用过高
  * 2、ps H -eo pid,tid,%cpu | grep 进程id (用ps命令进一步定位是哪个线程引起的cpu占用过高)
  * 3、jstack 进程id
- *
+ *   可以根据线程id找到有问题的线程,进一步定位到问题的源码行数
  * 进程数十进制演算为16进制的nid
+ *
+ * 定位到本代码正是因为while(true){} 导致cpu过高的问题
  * @createTime 2022年08月14日 17:41:41
  */
 public class Demo16 {
     public static void main(String[] args) {
         new Thread(null, () -> {
             System.out.println("1...");
-            while(true) {
-
-            }
+//            while(true) {
+//
+//            }
         }, "thread1").start();
 
 

5883ccd028348f24ab9182ec896f6174a5b36e23
xiaosu <2257346739@qq.com>
2022-08-16 22:25:34 +0800

    5.4常量池的定义，常量池，就是一张表，虚拟机指令根据这张常量表找到要执行的类名、方法名、参数类型、字面量等信息

--- jvm-stacks/src/main/java/com/onecbuying/jvm/t5/HelloWorld.java
+++ jvm-stacks/src/main/java/com/onecbuying/jvm/t5/HelloWorld.java
@@ -6,6 +6,7 @@ package com.onecbuying.jvm.t5;
  * @ClassName HelloWorld
  * @company 公司
  * @Description 二进制字节码（类基本信息，常量池，类方法定义，包含了虚拟机指令）
+ * 常量池，就是一张表，虚拟机指令根据这张常量表找到要执行的类名、方法名、参数类型、字面量等信息
  * @createTime 2022年08月16日 22:23:23
  */
 public class HelloWorld {

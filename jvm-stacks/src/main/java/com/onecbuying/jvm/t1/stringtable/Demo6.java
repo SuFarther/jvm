744cea37408fb7ea595a8c3a1d80b20c7c01c9a5
xiaosu <2257346739@qq.com>
2022-08-17 18:49:34 +0800

    StringTable位置在jdk1.8中存放在堆中

---
+++ jvm-stacks/src/main/java/com/onecbuying/jvm/t1/stringtable/Demo6.java
@@ -0,0 +1,31 @@
+package com.onecbuying.jvm.t1.stringtable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 苏东坡
+ * @version 1.0
+ * @ClassName Demo6
+ * @company 公司
+ * 演示 StringTable 位置
+ * 在jdk8下设置 -Xmx10m -XX:-UseGCOverheadLimit
+ * 在jdk6下设置 -XX:MaxPermSize=10m
+ * @createTime 2022年08月17日 18:48:48
+ */
+public class Demo6 {
+    public static void main(String[] args) throws InterruptedException {
+        List<String> list = new ArrayList<String>();
+        int i = 0;
+        try {
+            for (int j = 0; j < 260000; j++) {
+                list.add(String.valueOf(j).intern());
+                i++;
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(i);
+        }
+    }
+}

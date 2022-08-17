bfe8845579c047a3b72c1d2b5d48b3d33d484d4e
xiaosu <2257346739@qq.com>
2022-08-17 18:47:56 +0800

    StringTable 位置存放在jdk1.6中，存放在永久代中

---
+++ jvm6/src/main/java/com/onecbuying/jvm/Demo6.java
@@ -0,0 +1,31 @@
+package com.onecbuying.jvm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 苏东坡
+ * @version 1.0
+ * @ClassName Demo6
+ * @company 公司
+ * @Description 演示 StringTable 位置
+ * 在jdk8下设置 -Xmx10m -XX:-UseGCOverheadLimit
+ * 在jdk6下设置 -XX:MaxPermSize=10m
+ * @createTime 2022年08月16日 22:12:12
+ */
+public class Demo6 {
+    public static void main(String[] args) throws InterruptedException {
+        List<String> list = new ArrayList<String>();
+        int i = 0;
+        try {
+            for (int j = 0; j < 260000; j++) {
+                list.add(String.valueOf(j).intern());
+                i++;
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(i);
+        }
+    }
+}

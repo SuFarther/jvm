2ad97f64367b6ae8ec1783ebbf9b099399412fd3
xiaosu <2257346739@qq.com>
2022-08-18 17:16:10 +0800

    p41-直接内存(演示ByteBuffer作用)

---
+++ jvm8/src/main/java/com/onecbuying/jvm/t1/direct/Demo9.java
@@ -0,0 +1,68 @@
+package com.onecbuying.jvm.t1.direct;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author 苏东坡
+ * @version 1.0
+ * @ClassName Demo9
+ * @company 公司
+ * @Description p41-直接内存(演示ByteBuffer作用)
+ * @createTime 2022年08月18日 17:12:12
+ */
+public class Demo9 {
+    static final String FROM = "xx路径.mp4";
+    static final String TO = "xx路径.mp4";
+    static final int _1Mb = 1024 * 1024;
+
+    public static void main(String[] args) {
+        io(); // io 用时：1535.586957 1766.963399 1359.240226
+        directBuffer(); // directBuffer 用时：479.295165 702.291454 562.56592
+    }
+
+    private static void directBuffer() {
+        long start = System.nanoTime();
+        try (FileChannel from = new FileInputStream(FROM).getChannel();
+             FileChannel to = new FileOutputStream(TO).getChannel();
+        ) {
+            ByteBuffer bb = ByteBuffer.allocateDirect(_1Mb);
+            while (true) {
+                int len = from.read(bb);
+                if (len == -1) {
+                    break;
+                }
+                bb.flip();
+                to.write(bb);
+                bb.clear();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        long end = System.nanoTime();
+        System.out.println("directBuffer 用时：" + (end - start) / 1000_000.0);
+    }
+
+    private static void io() {
+        long start = System.nanoTime();
+        try (FileInputStream from = new FileInputStream(FROM);
+             FileOutputStream to = new FileOutputStream(TO);
+        ) {
+            byte[] buf = new byte[_1Mb];
+            while (true) {
+                int len = from.read(buf);
+                if (len == -1) {
+                    break;
+                }
+                to.write(buf, 0, len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        long end = System.nanoTime();
+        System.out.println("io 用时：" + (end - start) / 1000_000.0);
+    }
+}

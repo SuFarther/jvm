38827a142ae2303cc779ee667fc8924f11268b86
xiaosu <2257346739@qq.com>
2022-08-24 13:07:47 +0800

    P145(类加载-连接-准备)

---
+++ jvm8/src/main/java/com/onecbuying/jvm/t3/load/Load8.java
@@ -0,0 +1,20 @@
+package com.onecbuying.jvm.t3.load;
+
+/**
+ * @author 苏东坡
+ * @version 1.0
+ * @ClassName Demo8
+ * @company 公司
+ * @Description 演示 final 对静态变量的影响
+ * @createTime 2022年08月24日 13:04:04
+ */
+
+public class Load8 {
+
+    static int a;
+    static int b = 10;
+    static final int c = 20;
+    static final String d = "hello";
+    static final Object e = new Object();
+}
+
